55c2dd67d8624dfd310ecb3ae230efa79bb42b95
Steve Wolfe <steven.wolfe@coxautoinc.com>
2021-09-10 13:30:40 -0400

    stubbing in (hopefully) minimum example to reproduce issue

--- terraform/modules/shared_module/main.tf
+++ terraform/modules/shared_module/main.tf
@@ -5,3 +5,17 @@ terraform {
   }
 }
 
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+  version = "v2.16.0"
+
+  create_role = true
+
+  function_name = "Hello"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.7"
+
+  publish = true
+
+  source_path = "${path.module}/../../../src/"
+}
